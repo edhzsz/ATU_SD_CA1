3b71691c65c9c374c31e32dd7809569fd9e96829
Edgar Hernandez <edhzsz@gmail.com>
2022-11-06 23:31:34 +0000

    Add ShapesManager

---
+++ ShapesDisplay/src/edgs_atu/shapes/ShapesManager.java
@@ -0,0 +1,17 @@
+package edgs_atu.shapes;
+
+/**
+ * Manager of the shapes that will be displayed.
+ * Only shapes in this manager will be displayed.
+ */
+public class ShapesManager {
+    /**
+     * Indicates whether the bounding box of the shapes should be displayed.
+     */
+    private boolean displayBoundingBox;
+
+    /**
+     * Indicates whether the name of the shapes whould be displayed.
+     */
+    private boolean displayName;
+}
